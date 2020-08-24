86029d4235374672692284b031e4ab02bfd521b6
nithyaroslin <61905776+nithyaroslin@users.noreply.github.com>
2020-08-24 14:09:55 -0400

    Initial version

---
+++ conditions.py
@@ -0,0 +1,8 @@
+x = int(input("Enter a number:"))
+
+if x > 0:
+    print("x is positive")
+elif x < 0:
+    print("x is negative")
+else:
+    print("x is 0")
---
+++ variables.py
@@ -0,0 +1,6 @@
+a = 28
+b = 1.5
+c = "Hello!"
+d = True
+e = None
+print(f"a={a}, b={b}, c={c}, d={d}, e={e}")
